d433c729a6a664510f2f4d0a39f24f7d1348ba21
wmRD01 <wm.rd01@gmail.com>
2021-12-16 15:16:40 +0800

    1111

--- src/settings.19f20.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scene/login.fire",orientation:"",jsList:[],bundleVers:{internal:"3c089",resources:"cedf4",main:"f72fd"}};
